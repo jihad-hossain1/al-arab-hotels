b89ebc7abed2320bb852251bcde44e464b0c1348
jihad-hossain1 <jihadkhan934@gmail.com>
2023-04-24 13:29:59 +0600

    when user comments on a specific section then redirect a specific router

--- src/components/Book/Book.jsx
+++ src/components/Book/Book.jsx
@@ -1,16 +1,43 @@
 import React, { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Book = () => {
-  const user = null;
-  const navigate = useNavigate();
+  // const user = null;
+  const user = { email: "a" };
+  // const navigate = useNavigate();
 
-  useEffect(() => {
-    if (!user) {
-      navigate("/login");
-    }
-  }, []);
-  return <div>all nooks here ....</div>;
+  // useEffect(() => {
+  //   if (!user) {
+  //     navigate("/login");
+  //   }
+  // }, []);
+  return (
+    <div className="mx-10 my-10 py-10 min-h-screen">
+      <h2 className="text-center py-5 text-xl">all booking here ....</h2>
+      {user ? (
+        <form action="" className="">
+          <textarea
+            name=""
+            id=""
+            cols="100"
+            rows="5"
+            className="border border-cyan-600"
+          ></textarea>
+          <br />
+          <button className="border border-cyan-500 rounded px-2 bg-cyan-100 hover:bg-cyan-400">
+            Add your comment
+          </button>
+        </form>
+      ) : (
+        <Link
+          className="text-2xl text-center underline hover:text-cyan-500 hover:font-semibold"
+          to="/login"
+        >
+          Please Log-In First
+        </Link>
+      )}
+    </div>
+  );
 };
 
 export default Book;
